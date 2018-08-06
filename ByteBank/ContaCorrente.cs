6ac1a3fec339ea6c2a74033b1d7e1f4fcc7b909e
caiocmsouza <caiocesar.msouza@gmail.com>
2018-08-06 13:05:23 +0000

    projetoInicial

--- ByteBank/ContaCorrente.cs
+++ ByteBank/ContaCorrente.cs
@@ -1,26 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// using _05_ByteBank;
 
 namespace ByteBank
 {
     public class ContaCorrente
     {
-        private static int TaxaOperacao;
+        public Cliente Titular { get; set; }
 
         public static int TotalDeContasCriadas { get; private set; }
 
-        public Cliente Titular { get; set; }
 
-        public int ContadorSaquesNaoPermitidos { get; private set; }
-        public int ContadorTransferenciasNaoPermitidas { get; private set; }
+        private int _agencia;
+        public int Agencia
+        {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
 
-        public int Numero { get; }
-        public int Agencia { get; }
+                _agencia = value;
+            }
+        }
+        public int Numero { get; set; }
 
         private double _saldo = 100;
+
         public double Saldo
         {
             get
@@ -38,39 +47,25 @@ namespace ByteBank
             }
         }
 
+
         public ContaCorrente(int agencia, int numero)
         {
-            if (numero <= 0)
-            {
-                throw new ArgumentException("O argumento agencia deve ser maior que 0.", nameof(agencia));
-            }
-
-            if(numero <= 0)
-            {
-                throw new ArgumentException("O argumento numero deve ser maior que 0.", nameof(numero));
-            }
-
             Agencia = agencia;
             Numero = numero;
 
             TotalDeContasCriadas++;
-            TaxaOperacao = 30 / TotalDeContasCriadas;
         }
 
-        public void Sacar(double valor)
-        {
-            if (valor < 0)
-            {
-                throw new ArgumentException("Valor inválido para o saque.", nameof(valor));
-            }
 
+        public bool Sacar(double valor)
+        {
             if (_saldo < valor)
             {
-                ContadorSaquesNaoPermitidos++;
-                throw new SaldoInsuficienteException(Saldo, valor);
+                return false;
             }
 
             _saldo -= valor;
+            return true;
         }
 
         public void Depositar(double valor)
@@ -78,24 +73,17 @@ namespace ByteBank
             _saldo += valor;
         }
 
-        public void Transferir(double valor, ContaCorrente contaDestino)
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
         {
-            if (valor < 0)
+            if (_saldo < valor)
             {
-                throw new ArgumentException("Valor inválido para a transferência.", nameof(valor));
-            }
-            
-            try
-            {
-                Sacar(valor);
-            }
-            catch(SaldoInsuficienteException ex)
-            {
-                ContadorTransferenciasNaoPermitidas++;
-                throw new OperacaoFinanceiraException("Operação não realizada.", ex);
+                return false;
             }
 
+            _saldo -= valor;
             contaDestino.Depositar(valor);
+            return true;
         }
     }
 }
--- ByteBank/Properties/AssemblyInfo.cs
+++ ByteBank/Properties/AssemblyInfo.cs
@@ -2,9 +2,9 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// As informações gerais sobre um assembly são controladas por
-// conjunto de atributos. Altere estes valores de atributo para modificar as informações
-// associada a um assembly.
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("ByteBank")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,23 +14,23 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Definir ComVisible como false torna os tipos neste assembly invisíveis
-// para componentes COM. Caso precise acessar um tipo neste assembly de
-// COM, defina o atributo ComVisible como true nesse tipo.
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
-[assembly: Guid("d5b03f93-cdc4-4309-ae8e-ffd218108a1a")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e67ebbd9-038d-4176-b441-cd1e9c96fa7f")]
 
-// As informações da versão de um assembly consistem nos quatro valores a seguir:
+// Version information for an assembly consists of the following four values:
 //
-//      Versão Principal
-//      Versão Secundária 
-//      Número da Versão
-//      Revisão
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
 //
-// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
-// utilizando o "*" como mostrado abaixo:
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
